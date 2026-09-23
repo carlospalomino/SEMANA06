7630610bf382b08e9016f54312567bb456ea1ece
Carlos Palomino <carlospalomino@outlook.com>
2026-09-23 14:11:52 -0500

    Creacion Metodos Iniciales

---
+++ Semana06/src/modelo/Player.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author kepb
+ */
+public class Player {
+    private String nombre;
+    private int number;
+    private int age;
+    private double salary;
+    private String nameClub;
+
+    
+    // los metodos void no devuelven nada 
+    public void showInfo(){
+    
+    }
+    
+    // metodo que deuvelve valor 
+    public int play(){
+        return 0;
+    }
+    
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @param age the age to set
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * @return the salary
+     */
+    public double getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * @return the nameClub
+     */
+    public String getNameClub() {
+        return nameClub;
+    }
+
+    /**
+     * @param nameClub the nameClub to set
+     */
+    public void setNameClub(String nameClub) {
+        this.nameClub = nameClub;
+    }
+    
+    
+    
+}
